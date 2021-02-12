67dca760aabe45edc626b4e69ed7f563f3be4054
tran <fidtran@gmail.com>
2021-02-12 17:03:04 -0500

    adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCGStzL_sLnNyxYmXKkw9HBgf7SkEosxTw",
+	authDomain: "periodic-react.firebaseapp.com",
+	projectId: "periodic-react",
+	storageBucket: "periodic-react.appspot.com",
+	messagingSenderId: "819022660290",
+	appId: "1:819022660290:web:ff798d3a3045273ea2315b",
+	measurementId: "G-E0GG0HNX8B",
+};
